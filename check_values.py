Fix transpose + broadcast

check_values.py
@@ -55,7 +55,12 @@ onnx.save(model, './test.onnx')
 
 onnx_input = model.graph.input[0]
 input_shape = tuple(d.dim_value if d.dim_value != 0 else 1 for d in onnx_input.type.tensor_type.shape.dim)
-input = np.ones(input_shape, dtype=np.float32)
+# input = np.ones(input_shape, dtype=np.float32)
+s = 1
+for i in input_shape:
+    s *= i
+print(s)
+input = np.arange(s, dtype=np.float32).reshape(input_shape)
 
 sess = ort.InferenceSession("./test.onnx")
 ort_outputs = sess.run(names, {onnx_input.name: input}) 

simple_model.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 from torchvision.models import resnet50
 
 resnet = resnet50(pretrained=True)
@@ -7,23 +8,23 @@ resnet = resnet50(pretrained=True)
 class Model(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.register_parameter(name='weights', param=nn.Parameter(torch.randn(1,256,56,56)))
+        # self.register_parameter(name='weights', param=nn.Parameter(torch.randn(1,256,56,56)))
         self.blocks = nn.Sequential(
             # resnet.conv1,
             # nn.Conv2d(3, 64, 7, bias=False, padding=3, stride=2),
             # nn.Linear(2, 4, bias=False),
             # nn.ReLU(),
+            # nn.Upsample(scale_factor=2),
         )
         # self.blocks[0].weight.data.fill_(1.2)
 
     def forward(self, x):
-        # return self.blocks(x)
-        return x + self.weights
+        # return # self.blocks(x)
+        return x.permute(1, 0)
 
-
-# model = Model()
-model = resnet # Model()
-x = torch.ones(1,3,224,224)
+model = Model()
+# model = resnet # Model()
+x = torch.arange(0,16, dtype=torch.float32).view(4,4)
 # x = torch.ones(1,256,56,56)
 
 with torch.no_grad():
@@ -35,4 +36,3 @@ torch.onnx.export(
     "./simple_model.onnx",
     do_constant_folding=True,
 )
-